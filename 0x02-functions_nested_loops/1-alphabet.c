b6fe5a3912568fc28cd56c72247573c4d3f93496
chuks <chukkydave@gmail.com>
2022-03-16 06:51:39 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets from a to z in lowercase
+ * Return: returns void
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+	while(ch <= 'z'){
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('/n');
+	return;
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 ## Tasks for functions, nested loops
 
 * [A  program that prints _putchar, followed by a new line.](0-putchar.c)
+* [A  function that prints the alphabet, in lowercase, followed by a new line.](1-alphabet.c)
