42fa2a540c048b8f7097edc9c3cb54f176bb6eed
chuks <chukkydave@gmail.com>
2022-03-16 17:37:31 -0700

    betty doc

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * islower - it checks if the passed in char is lowercase
- * @c - input to take
+ * _islower - it checks if the passed in char is lowercase
+ * @c: input to take
  * Return: 1 if lowercase and 0 otherwise
  */
 int _islower(int c)
